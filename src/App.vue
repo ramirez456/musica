16b2d7e0bbacfdbce1f9c1d780c8f1f9bf0d3f87
Max Houston Ramirez Martel <maxr524@gmail.com>
2018-11-21 01:14:35 -0500

    modificando algunas rutas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://ramirez456.github.io/musica/assets/logo.png')
     h1 Max Music
     select(v-model="selectedCountry")
       option(v-for="country in countries" :value="country.value") {{country.name}}
